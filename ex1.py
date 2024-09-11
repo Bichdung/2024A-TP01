cf99c0a030cf5c3a0818c1313f4dc83f4bad72b7
Usalas Abbas <usalasabbas@gmail.com>
2024-09-11 16:22:39 -0400

    nouveau record

--- ex1.py
+++ ex1.py
@@ -3,5 +3,8 @@
 country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("Quel est le nom l'athlète ? ")
 date = input("Quel est la date du record ? ")
+discipline = input("Quel est la discipline du record ? ")
+categorie = input("Quel est la catégorie du record ? ")
+record = input("Quel est le record ? ")
 
-print(country, athlete, date)
+print("Nouveau Record : ", date, discipline, " - ", categorie, athlete, country, " - ", record )
