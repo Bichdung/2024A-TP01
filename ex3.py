e3e38442e9a8434f26dd8ec28453c37adf6ec2be
Usalas Abbas <usalasabbas@gmail.com>
2024-09-11 16:54:07 -0400

    exo 3 et 4 fait

--- ex3.py
+++ ex3.py
@@ -3,6 +3,6 @@
 import math
 g = 9.81
 speed = float(input("Quel est la vitesse initiale de la boule (en m/s)?"))
-angle = float(input("Quel est l'angle de lancement? (en degrés)"))
+angle = math.radians(float(input("Quel est l'angle de lancement? (en degrés)")))
 max_distance = ((speed**2 *math.sin(2 * angle)))/g
 print("La distance maximale en x est de", max_distance, "m.")
