c3aad3bf4f1f1e622a80638ef60de5b95ed77801
Usalas Abbas <usalasabbas@gmail.com>
2024-09-11 16:12:23 -0400

    exo 1 presque termine

--- ex1.py
+++ ex1.py
@@ -1,5 +1,7 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est le nom l'athlète ? ")
+date = input("Quel est la date du record ? ")
+
+print(country, athlete, date)
