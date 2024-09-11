e479d64509816964c08d32d54196dc85ca7b2bc5
Usalas Abbas <usalasabbas@gmail.com>
2024-09-11 17:15:39 -0400

    exo 3 et 4

--- ex3.py
+++ ex3.py
@@ -1,8 +1,8 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
 import math
-g = 9.81
+g = 9.8
 speed = float(input("Quel est la vitesse initiale de la boule (en m/s)?"))
 angle = math.radians(float(input("Quel est l'angle de lancement? (en degrés)")))
 max_distance = ((speed**2 *math.sin(2 * angle)))/g
-print("La distance maximale en x est de", max_distance, "m.")
+print("La distance maximale en x est de", round(max_distance, 1), "m.")
--- ex4.py
+++ ex4.py
@@ -3,4 +3,17 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = input(" What's the battery level ?")
+battery_level = int(input(" What's the battery level ?"))
+resultat =0
+
+if battery_level > 50 : resultat = battery_level*2
+
+elif battery_level > 25 : resultat = battery_level*0.5
+
+elif battery_level > 10 : resultat = battery_level*0.1
+
+elif battery_level > 5 : resultat = battery_level*2.5
+
+elif battery_level > 0 : resultat = battery_level*6
+
+print(resultat, " km")
